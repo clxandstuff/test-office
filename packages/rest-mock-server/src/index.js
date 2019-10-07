10926acddf5f05884d7e70bfc43c5a84ea80835d
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2019-10-08 00:03:38 +0200

    feat: match by path

--- packages/rest-mock-server/src/index.js
+++ packages/rest-mock-server/src/index.js
@@ -1,15 +1,39 @@
 const { Observable } = require('rxjs');
+const { map } = require('rxjs/operators');
 const express = require('express');
 const cors = require('cors');
 
+function matchPath(mock, req) {
+  if (!mock.request || !mock.request.path) {
+    return true;
+  }
+
+  return mock.request.path === req.path;
+}
+
+function matchMock(req, mocks) {
+  if (mocks.length < 1) {
+    return undefined;
+  }
+
+  return mocks.find(mock => {
+    return matchPath(mock, req);
+  });
+}
+
+function getRequestCriteria(req) {
+  return {
+    path: req.url
+  };
+}
+
 exports.start = function start(port = 3000, mocks = []) {
-  new Observable(subscriber => {
+  const request$ = Observable.create(subscriber => {
     const app = express();
-    app.use(cors()).use((req, res, next) =>
+    app.use(cors()).use((req, res) =>
       subscriber.next({
         req,
-        res,
-        next
+        res
       })
     );
     const server = app.listen(port, () => {
@@ -21,12 +45,24 @@ exports.start = function start(port = 3000, mocks = []) {
         throw new Error(`Port ${err.port} is in use. Choose different port.`);
       }
     });
-  }).subscribe(({ res }) => {
-    if (mocks.length === 0) {
-      res.status(400).end('rest-mock-server: mocks are empty');
+  });
+
+  const searchMock = map(({ req, res }) => {
+    const requestCriteria = getRequestCriteria(req);
+    const matchedMock = matchMock(requestCriteria, mocks);
+
+    return {
+      mock: matchedMock,
+      res
+    };
+  });
+
+  request$.pipe(searchMock).subscribe(({ mock, res }) => {
+    if (mock) {
+      res.json(JSON.parse(mock.response.body));
       return;
     }
 
-    res.json(mocks[0]);
+    res.status(404).end('rest-mock-server: mocks not found');
   });
 };
--- packages/rest-mock-server/test/index.js
+++ packages/rest-mock-server/test/index.js
@@ -2,8 +2,24 @@ const restMockServer = require('../src');
 
 restMockServer.start(3000, [
   {
+    request: {
+      path: '/a'
+    },
     response: {
-      body: JSON.stringify({ hello: 'world' })
+      body: JSON.stringify({ message: 'mock for /a' })
+    }
+  },
+  {
+    request: {
+      path: '/b'
+    },
+    response: {
+      body: JSON.stringify({ message: 'mock for /b' })
     }
   }
+  // {
+  //   response: {
+  //     body: JSON.stringify({ message: 'mock for all paths' })
+  //   }
+  // }
 ]);
